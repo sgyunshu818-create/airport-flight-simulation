a3b58616a54af3dbe48c43417a7a473271dcf29a
yunshu <sgyunshu818@gmail.com>
2026-05-29 11:06:40 +0800

    chore: create spring boot project skeleton

---
+++ src/main/java/com/example/airportsimulation/AirportSimulationApplication.java
@@ -0,0 +1,11 @@
+package com.example.airportsimulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirportSimulationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AirportSimulationApplication.class, args);
+    }
+}
